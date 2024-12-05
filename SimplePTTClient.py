7074a18ebabc5e6448697151b5524679dce50808
Tommy Chiang <ty1208chiang@gmail.com>
2024-12-06 02:54:22 +0800

    Format code

--- SimplePTTClient.py
+++ SimplePTTClient.py
@@ -3,7 +3,7 @@ import time
 
 
 TIMEOUT = 0.01
-TERM_ENCODING = 'big5'
+TERM_ENCODING = "big5"
 
 
 class PTTClient:
@@ -11,90 +11,87 @@ class PTTClient:
         self.conn = create_connection(host, origin=origin, timeout=TIMEOUT)
 
     def recv_data(self):
-        data = ''
+        data = ""
         while True:
             try:
-                data += self.conn.recv().decode(TERM_ENCODING, 'ignore')
+                data += self.conn.recv().decode(TERM_ENCODING, "ignore")
                 time.sleep(TIMEOUT)
             except WebSocketTimeoutException:
                 break
         time.sleep(1)
         return data
 
-
     def send_data(self, data):
         self.conn.send(data.encode(TERM_ENCODING), ABNF.OPCODE_BINARY)
         return
 
-
     def login(self, username, password, kickOther=False):
         frame = self.recv_data()
-        self.send_data(username+'\r\n')
+        self.send_data(username + "\r\n")
         frame = self.recv_data()
-        self.send_data(password+'\r\n')
+        self.send_data(password + "\r\n")
         frame = self.recv_data()
-        while '密碼正確' not in frame:
+        while "密碼正確" not in frame:
             frame += self.recv_data()
-            if '密碼不對喔' in frame:
+            if "密碼不對喔" in frame:
                 return False
         frame = self.recv_data()
 
-        if '您想刪除其他重複登入的連線嗎' in frame:
-            self.send_data('y\r\n' if kickOther else 'n\r\n')
+        if "您想刪除其他重複登入的連線嗎" in frame:
+            self.send_data("y\r\n" if kickOther else "n\r\n")
             frame = self.recv_data()
-        if '更新與同步線上使用者及好友名單' in frame:
+        if "更新與同步線上使用者及好友名單" in frame:
             frame = self.recv_data()
-        if '請按任意鍵繼續' in frame:
-            self.send_data('a')
+        if "請按任意鍵繼續" in frame:
+            self.send_data("a")
             frame = self.recv_data()
-        if '刪除以上錯誤嘗試的記錄' in frame:
-            self.send_data('y\r\n')
+        if "刪除以上錯誤嘗試的記錄" in frame:
+            self.send_data("y\r\n")
             frame = self.recv_data()
-        if '您有一篇文章尚未完成' in frame:
-            self.send_data('q\r\n')
+        if "您有一篇文章尚未完成" in frame:
+            self.send_data("q\r\n")
             frame = self.recv_data()
-        if '您保存信件數目' in frame and '超出上限' in frame:
-            self.send_data('a')
+        if "您保存信件數目" in frame and "超出上限" in frame:
+            self.send_data("a")
             frame = self.recv_data()
-            self.send_data('q')
+            self.send_data("q")
             frame = self.recv_data()
         cnt = 0
-        while '主功能表' not in frame and cnt < 10:
-            self.send_data(' ')
+        while "主功能表" not in frame and cnt < 10:
+            self.send_data(" ")
             frame = self.recv_data()
             cnt += 1
-        if '主功能表' not in frame:
+        if "主功能表" not in frame:
             return False
         return True
 
-
     def post(self, board, title, content):
-        self.send_data('s')
+        self.send_data("s")
         frame = self.recv_data()
-        self.send_data(board+'\r\n')
+        self.send_data(board + "\r\n")
         frame = self.recv_data()
-        if '動畫播放中' in frame:
-            self.send_data('q')
+        if "動畫播放中" in frame:
+            self.send_data("q")
             frame = self.recv_data()
-        if '請按任意鍵繼續' in frame:
-            self.send_data('q')
+        if "請按任意鍵繼續" in frame:
+            self.send_data("q")
             frame = self.recv_data()
-        self.send_data('\x10')
-        self.send_data('\r\n')
-        self.send_data(title+'\r\n')
+        self.send_data("\x10")
+        self.send_data("\r\n")
+        self.send_data(title + "\r\n")
         for char in content:
             self.send_data(char)
         frame = self.recv_data()
-        self.send_data('\x18')
+        self.send_data("\x18")
         frame = self.recv_data()
-        while '檔案處理' not in frame:
+        while "檔案處理" not in frame:
             frame += self.recv_data()
-        self.send_data('s\r\n')
+        self.send_data("s\r\n")
         frame = self.recv_data()
 
-        if '簽名檔' in frame:
-            self.send_data('0\r\n')
+        if "簽名檔" in frame:
+            self.send_data("0\r\n")
             frame = self.recv_data()
-        if '請按任意鍵繼續' in frame:
-            self.send_data('a')
+        if "請按任意鍵繼續" in frame:
+            self.send_data("a")
             frame = self.recv_data()
--- weather.py
+++ weather.py
@@ -7,32 +7,60 @@ from getpass import getpass
 import requests
 from SimplePTTClient import PTTClient
 
+
 def process_argument():
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest='cmd', metavar='cmd', required=True)
+    subparsers = parser.add_subparsers(
+        dest="cmd", metavar="cmd", required=True
+    )
 
-    from_config = subparsers.add_parser('config', help='使用設定檔匯入設定')
-    from_config.add_argument('config_file', type=argparse.FileType('r'), help='設定檔位置')
+    from_config = subparsers.add_parser("config", help="使用設定檔匯入設定")
+    from_config.add_argument(
+        "config_file", type=argparse.FileType("r"), help="設定檔位置"
+    )
 
-    from_args = subparsers.add_parser('exec', help='從命令列輸入設定')
-    from_args.add_argument('-u', '--username', type=str, help='登入帳號', default=None)
-    from_args.add_argument('-p', '--password', type=str, help='登入密碼', default=None)
-    from_args.add_argument('-k', '--apikey', type=str, help='中央氣象局授權碼', default=None)
-    from_args.add_argument('-b', '--board', type=str, help='發文看板', default=None)
-    from_args.add_argument('-c', '--host', type=str, help='登入主機', default='wss://ws.ptt2.cc/bbs')
-    from_args.add_argument('-o', '--origin', type=str, help='登入來源', default='https://term.ptt2.cc')
+    from_args = subparsers.add_parser("exec", help="從命令列輸入設定")
+    from_args.add_argument(
+        "-u", "--username", type=str, help="登入帳號", default=None
+    )
+    from_args.add_argument(
+        "-p", "--password", type=str, help="登入密碼", default=None
+    )
+    from_args.add_argument(
+        "-k", "--apikey", type=str, help="中央氣象局授權碼", default=None
+    )
+    from_args.add_argument(
+        "-b", "--board", type=str, help="發文看板", default=None
+    )
+    from_args.add_argument(
+        "-c",
+        "--host",
+        type=str,
+        help="登入主機",
+        default="wss://ws.ptt2.cc/bbs",
+    )
+    from_args.add_argument(
+        "-o",
+        "--origin",
+        type=str,
+        help="登入來源",
+        default="https://term.ptt2.cc",
+    )
     return parser.parse_args()
 
 
 def CWB_data(dataid, apikey):
     return requests.get(
-        f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/{dataid}\
-        ?Authorization={apikey}&format=JSON'
-    ).json()['cwaopendata']['dataset']
+        f"https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/{dataid}"
+        + f"?Authorization={apikey}&format=JSON"
+    ).json()["cwaopendata"]["dataset"]
 
 
 def datetime2str(dt):
-    return f'{dt.year}年{dt.month:02d}月{dt.day:02d}日{dt.hour:02d}時{dt.minute:02d}分'
+    return (
+        f"{dt.year}年{dt.month:02d}月{dt.day:02d}日"
+        + f"{dt.hour:02d}時{dt.minute:02d}分"
+    )
 
 
 def tcolor(s):
@@ -69,71 +97,87 @@ def rcolor(s):
 
 def generate_post_content(data):
     issue_time = datetime.strptime(
-        data['datasetInfo']['issueTime'],
-        '%Y-%m-%dT%H:%M:%S%z'
+        data["datasetInfo"]["issueTime"], "%Y-%m-%dT%H:%M:%S%z"
     )
     start_time = datetime.strptime(
-        data['location'][0]['weatherElement'][0]['time'][0]['startTime'],
-        '%Y-%m-%dT%H:%M:%S%z'
+        data["location"][0]["weatherElement"][0]["time"][0]["startTime"],
+        "%Y-%m-%dT%H:%M:%S%z",
     )
     end_time = datetime.strptime(
-        data['location'][0]['weatherElement'][0]['time'][0]['endTime'],
-        '%Y-%m-%dT%H:%M:%S%z'
+        data["location"][0]["weatherElement"][0]["time"][0]["endTime"],
+        "%Y-%m-%dT%H:%M:%S%z",
     )
 
-    content = f'''發布時間：{datetime2str(issue_time)}
+    content = f"""發布時間：{datetime2str(issue_time)}
 有效時間：{datetime2str(start_time)}起至{datetime2str(end_time)}
 
 預報分區　　　　　天　　　　氣　　　　　雨率　氣溫(攝氏)
 
-'''
-    for pos in data['location']:
+"""
+    for pos in data["location"]:
         weather_element = {}
-        for sub in pos['weatherElement']:
-            weather_element[sub['elementName']] = sub['time'][0]
+        for sub in pos["weatherElement"]:
+            weather_element[sub["elementName"]] = sub["time"][0]
 
-        content += '＊{name}　{descript}{rain}％　　{min_t} - {max_t}\n'.format(
-            name=pos['locationName'],
-            descript=weather_element['Wx']['parameter']['parameterName'].ljust(15, '　'),
-            max_t=tcolor(weather_element['MaxT']['parameter']['parameterName'].rjust(2, ' ')),
-            min_t=tcolor(weather_element['MinT']['parameter']['parameterName'].rjust(2, ' ')),
-            rain=rcolor(weather_element['PoP']['parameter']['parameterName'].rjust(3, ' '))
+        content += (
+            "＊{name}　{descript}{rain}％　　{min_t} - {max_t}\n".format(
+                name=pos["locationName"],
+                descript=weather_element["Wx"]["parameter"][
+                    "parameterName"
+                ].ljust(15, "　"),
+                max_t=tcolor(
+                    weather_element["MaxT"]["parameter"][
+                        "parameterName"
+                    ].rjust(2, " ")
+                ),
+                min_t=tcolor(
+                    weather_element["MinT"]["parameter"][
+                        "parameterName"
+                    ].rjust(2, " ")
+                ),
+                rain=rcolor(
+                    weather_element["PoP"]["parameter"]["parameterName"].rjust(
+                        3, " "
+                    )
+                ),
+            )
         )
-    content += '''
+    content += """
 ＊備註：各縣市預報係以各縣市政府所在地附近為預報參考位置。
 
 ---資料來源:中央氣象局---
----  Coded By oToToT  ---'''
-    content = content.replace('\n', '\r\n')
+---  Coded By oToToT  ---"""
+    content = content.replace("\n", "\r\n")
     return content
 
 
 def generate_post_title(data):
-    tstamp = datetime.strptime(data['datasetInfo']['issueTime'],
-                               '%Y-%m-%dT%H:%M:%S%z')
-    stage = ''
+    tstamp = datetime.strptime(
+        data["datasetInfo"]["issueTime"], "%Y-%m-%dT%H:%M:%S%z"
+    )
+    stage = ""
     if tstamp.hour < 12:
-        stage = '早上'
+        stage = "早上"
     elif tstamp.hour == 12:
-        stage = '中午'
+        stage = "中午"
     else:
-        stage = '晚上'
-    return f'[預報] {tstamp.year}/{tstamp.month:02d}/{tstamp.day:02d} {stage}'
+        stage = "晚上"
+    return f"[預報] {tstamp.year}/{tstamp.month:02d}/{tstamp.day:02d} {stage}"
 
 
 def main():
     arg = process_argument()
 
-    if arg.cmd == 'config':
+    if arg.cmd == "config":
         config = json.load(arg.config_file)
         arg.config_file.close()
-        username = config.get('username', None)
-        password = config.get('password', None)
-        board = config.get('board', None)
-        apikey = config.get('apikey', None)
-        host = config.get('host', 'wss://ws.ptt2.cc/bbs')
-        origin = config.get('origin', 'https://term.ptt2.cc')
-    elif arg.cmd == 'exec':
+        username = config.get("username", None)
+        password = config.get("password", None)
+        board = config.get("board", None)
+        apikey = config.get("apikey", None)
+        host = config.get("host", "wss://ws.ptt2.cc/bbs")
+        origin = config.get("origin", "https://term.ptt2.cc")
+    elif arg.cmd == "exec":
         username = arg.username
         password = arg.password
         board = arg.board
@@ -142,15 +186,15 @@ def main():
         origin = arg.origin
 
     if not username:
-        username = input('登入帳號: ')
+        username = input("登入帳號: ")
     if not password:
-        password = getpass('登入密碼: ')
+        password = getpass("登入密碼: ")
     if not board:
-        board = input('發文看板: ')
+        board = input("發文看板: ")
     if not apikey:
-        apikey = getpass('中央氣象局授權碼: ')
+        apikey = getpass("中央氣象局授權碼: ")
 
-    data = CWB_data('F-C0032-001', apikey)
+    data = CWB_data("F-C0032-001", apikey)
     content = generate_post_content(data)
     title = generate_post_title(data)
 
@@ -161,5 +205,5 @@ def main():
     client.post(board, title, content)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
